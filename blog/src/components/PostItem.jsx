6ec0b68528ea3c0153f1919e70adb0a700f4d7f6
volodymyr198 <volodymyr.b.198@gmail.com>
2023-06-24 19:29:45 +0300

    added visualization if there is no post

--- blog/src/components/PostItem.jsx
+++ blog/src/components/PostItem.jsx
@@ -10,46 +10,53 @@ const { isLoading } = useSelector(state => state.auth);
 
     return (
         <>
-            {isLoading && (<IsLoading/>)}
-            {post &&(<Link to={`/${post._id}`}>
-                <div className="flex flex-col basis-1/4 flex-grow">
-                    <div
-                        className={
-                            post.imgUrl ? 'flex rounded-sm h-80' : 'flex rounded-sm'
-                        }
-                    >
-                        {post.imgUrl && (
-                            <img
-                                src={`http://localhost:3002/${post.imgUrl}`}
-                                alt="img"
-                                className="w-full object-cover"
-                            />
-                        )}
-                    </div>
-                    <div className="flex justify-between items-center pt-2">
-                        <div className="text-xs text-white opacity-50">
-                            {post.username}
+            {isLoading && <IsLoading />}
+            {post && (
+                <Link to={`/${post._id}`}>
+                    <div className="flex flex-col basis-1/4 flex-grow">
+                        <div
+                            className={
+                                post.imgUrl
+                                    ? 'flex rounded-sm h-80'
+                                    : 'flex rounded-sm'
+                            }
+                        >
+                            {post.imgUrl && (
+                                <img
+                                    src={`http://localhost:3002/${post.imgUrl}`}
+                                    alt="img"
+                                    className="w-full object-cover"
+                                />
+                            )}
                         </div>
-                        <div className="text-xs text-white opacity-50">
-                            <Moment date={post.createdAt} format="D MMM YYYY" />
+                        <div className="flex justify-between items-center pt-2">
+                            <div className="text-xs text-white opacity-50">
+                                {post.username}
+                            </div>
+                            <div className="text-xs text-white opacity-50">
+                                <Moment
+                                    date={post.createdAt}
+                                    format="D MMM YYYY"
+                                />
+                            </div>
+                        </div>
+                        <div className="text-white text-xl">{post.title}</div>
+                        <p className="text-white opacity-60 text-xs pt-4">
+                            {post.text}
+                        </p>
+
+                        <div className="flex gap-3 items-center mt-2">
+                            <button className="flex items-center justify-center gap-2 text-xs text-white opacity-50">
+                                <AiFillEye /> <span>{post.views}</span>
+                            </button>
+                            <button className="flex items-center justify-center gap-2 text-xs text-white opacity-50">
+                                <AiOutlineMessage />{' '}
+                                <span>{post.comments?.length || 0}</span>
+                            </button>
                         </div>
                     </div>
-                    <div className="text-white text-xl">{post.title}</div>
-                    <p className="text-white opacity-60 text-xs pt-4">
-                        {post.text}
-                    </p>
-    
-                    <div className="flex gap-3 items-center mt-2">
-                        <button className="flex items-center justify-center gap-2 text-xs text-white opacity-50">
-                            <AiFillEye /> <span>{post.views}</span>
-                        </button>
-                        <button className="flex items-center justify-center gap-2 text-xs text-white opacity-50">
-                            <AiOutlineMessage />{' '}
-                            <span>{post.comments?.length || 0}</span>
-                        </button>
-                    </div>
-                </div>
-            </Link>)}
+                </Link>
+            )}
         </>
     );
 };
--- blog/src/pages/PostsPage.jsx
+++ blog/src/pages/PostsPage.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import axios from '../utils/axios';
 import { PostItem } from '../components/PostItem';
+import { IsLoading } from '../components/IsLoading';
 
 export const PostsPage = () => {
     const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const fetchMyPosts = useCallback(async () => {
         try {
@@ -12,6 +14,8 @@ export const PostsPage = () => {
             setPosts(data);
         } catch (error) {
             console.log(error.message);
+        } finally {
+            setIsLoading(false);
         }
     }, []);
 
@@ -21,9 +25,14 @@ export const PostsPage = () => {
 
     return (
         <div className="w-1/2 mx-auto py-10 flex flex-col gap-10">
-            {posts?.map((post, idx) => (
-                <PostItem post={post} key={idx} />
-            ))}
+            {isLoading ? (<IsLoading />
+            ) : posts.length > 0 ? (
+                posts.map((post, idx) => <PostItem post={post} key={idx} />)
+            ) : (
+                <div className="text-xl text-center text-white py-10">
+                    No posts have been created yet :(
+                </div>
+            )}
         </div>
     );
 };
